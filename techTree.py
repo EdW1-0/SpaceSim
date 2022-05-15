5fad8697d56aa5935934d76547f436b3c9984bf9
EdW1-0 <edmundwarrick@hotmail.co.uk>
2022-05-16 00:40:18 +0100

    Added init and basic UML

--- techTree.py
+++ techTree.py
@@ -1,4 +1,5 @@
 
 
 class TechTree:
-    pass
+    def __init__(self):
+        pass
--- test_techTree.py
+++ test_techTree.py
@@ -8,6 +8,14 @@ class TestTechTree(unittest.TestCase):
         self.assertNotEqual(techTree, False)
         self.assertNotEqual(techTree.TechTree, False)
 
+    def test_techTreeInitialise(self):
+        self.assertTrue(self.techTree)
+
+    def setUp(self):
+        self.techTree = techTree.TechTree()
+
+    def test_techTreeAccessTree(self):
+        self.assertTrue(self.techTree.tree)
 
 
 
