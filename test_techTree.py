889c88f99cd26ac6d0e659635792a9ebf4afb976
EdW1-0 <edmundwarrick@hotmail.co.uk>
2022-05-15 23:16:18 +0100

    Trivial implementation of tech tree

--- test_techTree.py
+++ test_techTree.py
@@ -1,2 +1,16 @@
 import unittest
 
+import techTree
+
+class TestTechTree(unittest.TestCase):
+    
+    def test_techTreeImport(self):
+        self.assertNotEqual(techTree, False)
+        self.assertNotEqual(techTree.TechTree, False)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
