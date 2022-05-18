f7601cc835a5fd95613bd014179d164833aaaf0f
EdW1-0 <edmundwarrick@hotmail.co.uk>
2022-05-19 00:23:49 +0100

    Added json loading of tech tree and basic lookup

--- techTree.py
+++ techTree.py
@@ -1,5 +1,22 @@
+import os
+import json
+
+
+import techNode
 
 
 class TechTree:
-    def __init__(self):
-        pass
+    def __init__(self, jsonPath):
+        jsonFile = open(jsonPath, "r")
+        jsonTechs = json.load(jsonFile)
+
+        jsonNodes = jsonTechs["Technologies"]
+        self.nodes = [techNode.TechNode(node["id"]) for node in jsonNodes]
+        self.totalNodes = len(self.nodes)
+
+    def nodeById(self, id):
+        if not isinstance(id, int):
+            raise TypeError
+        elif id < 0:
+            raise ValueError
+        return self.nodes[id]
--- test_techTree.py
+++ test_techTree.py
@@ -1,21 +1,47 @@
 import unittest
 
 import techTree
+import techNode
 
-class TestTechTree(unittest.TestCase):
+class TestTechTreeLoading(unittest.TestCase):
     
     def test_techTreeImport(self):
         self.assertNotEqual(techTree, False)
         self.assertNotEqual(techTree.TechTree, False)
 
     def test_techTreeInitialise(self):
-        self.assertTrue(self.techTree)
+        self.assertTrue(techTree.TechTree("json/Technologies.json"))
+        with self.assertRaises(FileNotFoundError):
+            techTree.TechTree("")
+
+
+
+
+class TestTechTreeInteraction(unittest.TestCase):
 
     def setUp(self):
-        self.techTree = techTree.TechTree()
+        self.techTree = techTree.TechTree("json/Technologies.json")
+        
 
-    def test_techTreeAccessTree(self):
-        self.assertTrue(self.techTree.tree)
+    def test_techTreeAccessBadNode(self):
+        with self.assertRaises(ValueError):
+            self.techTree.nodeById(-1)
+        with self.assertRaises(TypeError):
+            self.techTree.nodeById("Foo")
+
+    def test_techTreeAccessNode(self):
+        self.assertNotEqual(self.techTree.nodeById(0), None)
+        self.assertTrue(isinstance(self.techTree.nodeById(0), techNode.TechNode))
+        self.assertEqual(self.techTree.nodeById(0), self.techTree.nodeById(0))
+        self.assertNotEqual(self.techTree.nodeById(0), self.techTree.nodeById(1))
+
+    def test_techTreeAccessNodeId(self):
+        self.assertEqual(self.techTree.nodeById(0).id, 0)
+        self.assertEqual(self.techTree.nodeById(1).id, 1)
+
+    def test_techTreeTotalNodes(self):
+        self.assertNotEqual(self.techTree.totalNodes, 0)
+        self.assertEqual(self.techTree.totalNodes, 7)
 
 
 
