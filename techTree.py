d567990c811ccc545915638840d1638a6932a4b1
EdW1-0 <edmundwarrick@hotmail.co.uk>
2022-05-22 23:12:10 +0100

    Added lots of validation on TechTree

--- techTree.py
+++ techTree.py
@@ -11,8 +11,35 @@ class TechTree:
         jsonTechs = json.load(jsonFile)
 
         jsonNodes = jsonTechs["Technologies"]
-        self.nodes = [techNode.TechNode(**node) for node in jsonNodes]
+
+        self.nodes = {node["id"]: techNode.TechNode(**node) for node in jsonNodes}
         self.totalNodes = len(self.nodes)
+
+        # Validate no duplicate ids 
+        assert(self.totalNodes == len(jsonNodes))
+
+        # Validate ancestors - check every ancestor points to a real key
+        for node in self.nodes:
+            for ancestor in self.nodeById(node).ancestors:
+                assert(ancestor in self.nodes)
+
+        # Not optimal - we recheck many nodes by setting up checked afresh on every node. Probably a faster way to do this, but 
+        # we only need to do this check once, at load time, so may be tolerable.
+        def cycleCheck(node, checked):
+            assert(node.id not in checked)
+            checked.add(node.id)
+
+            for ancestor in node.ancestors:
+                # Note we don't care if we reach an ancestor by 2 independent routes,
+                # so don't keep checked state from ancestor branches, only descendents
+                branchChecked = checked.copy()
+                cycleCheck(self.nodeById(ancestor), branchChecked)
+        
+        for node in self.nodes.values():
+            checked = set()
+            cycleCheck(node, checked)
+
+
 
     def nodeById(self, id):
         if not isinstance(id, int):
@@ -20,3 +47,10 @@ class TechTree:
         elif id < 0:
             raise ValueError
         return self.nodes[id]
+
+    def ancestorsOfId(self, id):
+        node = self.nodeById(id)
+        return [self.nodeById(ancestor) for ancestor in node.ancestors]
+
+    def descendentsOfId(self, id):
+        return [node for node in self.nodes.values() if id in node.ancestors]
