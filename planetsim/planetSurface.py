6aba966e10eef9b0812e49cd32643156da93c7d8
EdW1-0 <edmundwarrick@hotmail.co.uk>
2022-06-28 23:42:02 +0100

    PlanetSurface - great circle functions and intersection test

--- planetsim/planetSurface.py
+++ planetsim/planetSurface.py
@@ -1,7 +1,8 @@
 import json
 
 from planetsim.surfaceRegion import SurfaceRegion
-from planetsim.surfacePoint import SurfacePoint
+from planetsim.surfacePoint import SurfacePoint, dot, cross, magnitude, latLong
+import math
 
 EARTH_RADIUS = 6371000
 
@@ -23,4 +24,44 @@ class PlanetSurface:
             region = SurfaceRegion(r["id"], anchor, borders)
             self.regions.append(region)
 
+    def gcAngle(self, point1, point2):
+        sine = magnitude(cross(point1.vector(), point2.vector()))
+        cosine = dot(point1.vector(), point2.vector())
+        return math.atan2(sine, cosine)
+
+    def gcAngleHav(self, point1, point2):
+        phi1 = point1.latitude * math.pi/180.0
+        phi2 = point2.latitude * math.pi/180.0
+        deltaphi = (point2.latitude - point1.latitude) * math.pi/180.0
+        deltalambda = (point2.longitude - point1.longitude) * math.pi/180.0
+        a = math.sin(deltaphi/2)*math.sin(deltaphi/2) + math.cos(phi1)*math.cos(phi2) + math.sin(deltalambda/2)*math.sin(deltalambda/2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        return c
+
+    def gcDistance(self, point1, point2):
+        return self.radius * self.gcAngle(point1, point2)
+   
+    def gc(self, path):
+        v1 = path[0].vector()
+        v2 = path[1].vector()
+        c = cross(v1, v2)
+        mag = magnitude(c)
+        gc = tuple(i/mag for i in c)
+        return gc
+
+    def gcIntersections(self, path1, path2):
+        gc1 = self.gc(path1)
+        gc2 = self.gc(path2)
+        i1 = cross(gc1, gc2)
+        i2 = cross(gc2, gc1)
+        return (i1, i2)
+
+    def pathsIntersect(self, path1, path2):
+        intersections = tuple(latLong(i) for i in self.gcIntersections(path1, path2))
+        intersect = False
+        for i in intersections:
+            if i.longitude > path1[0].longitude and i.longitude < path1[1].longitude:
+                if i.longitude > path2[0].longitude and i.longitude < path2[1].longitude:
+                    intersect = True
+        return intersect
 
--- tests/test_planet/test_planetSurface.py
+++ tests/test_planet/test_planetSurface.py
@@ -1,7 +1,9 @@
 import unittest
 
+import math
+
 from planetsim.planetSurface import PlanetSurface, EARTH_RADIUS
-from planetsim.surfacePoint import SurfacePoint
+from planetsim.surfacePoint import SurfacePoint, latLong, normalisePoint
 
 class TestPlanetSurface(unittest.TestCase):
     def testPlanetSurfaceModule(self):
@@ -31,11 +33,68 @@ class TestPlanetSurfaceLoading(unittest.TestCase):
         for b in self.ps.regions[0].borders:
             self.assertIsInstance(b, SurfacePoint)
 
-    def testGreatCircleDistance(self):
-        pass #self.assertEqual()
+class TestGreatCircleGeodetics(unittest.TestCase):
+    def setUp(self):
+        self.ps = PlanetSurface("test_json/test_surfaces/single_region.json")
+        self.twor = PlanetSurface("test_json/test_surfaces/2_hemispheres.json")
 
-    def testPathsIntersect(self):
-        pass
+    def testGreatCircleAngle(self):
+        self.assertEqual(self.ps.gcAngle(SurfacePoint(0.0, 0.0), SurfacePoint(0.0, 0.0)), 0.0)
+        self.assertEqual(self.ps.gcAngle(SurfacePoint(0.0, 0.0), SurfacePoint(90.0, 0.0)), math.pi/2)
+        self.assertEqual(self.ps.gcAngle(SurfacePoint(0.0, 0.0), SurfacePoint(0.0, 180.0)), math.pi)
+
+    def testGreatCircleDistance(self):
+        self.assertAlmostEqual(self.ps.gcDistance(SurfacePoint(90.0, 0.0), SurfacePoint(0.0,0.0)), 10007543, delta = 1)
+        self.assertAlmostEqual(self.ps.gcDistance(SurfacePoint(0.0, 0.0), SurfacePoint(0.0,180.0)), 20015086, delta = 1)
+        london = SurfacePoint(51.5,0.1)
+        oxford = SurfacePoint(51.7,1.2)
+        newYork = SurfacePoint(40.7, 74.0)
+        self.assertAlmostEqual(self.ps.gcDistance(london, newYork), 5585000, delta = 20000)
+        self.assertAlmostEqual(self.ps.gcDistance(london, oxford), 81890, delta = 5000)
+
+class TestIntersectionTesting(unittest.TestCase):
+    def setUp(self):
+        self.ps = PlanetSurface("test_json/test_surfaces/single_region.json")
+        self.twor = PlanetSurface("test_json/test_surfaces/2_hemispheres.json")
+
+    def testPathsIntersectionPoints(self):
+        path1 = (SurfacePoint(0,0), SurfacePoint(0,90))
+        path2 = (SurfacePoint(0,0), SurfacePoint(90,0))
+        self.assertEqual(self.ps.gcIntersections(path1, path2), ((1,0,0),(-1,0,0)))
+        path1 = (SurfacePoint(50, 20), SurfacePoint(-50, 80))
+        path2 = (SurfacePoint(-50, 20), SurfacePoint(50, 80))
+        ints = tuple(normalisePoint(latLong(i)) for i in self.ps.gcIntersections(path1, path2))
+        self.assertAlmostEqual(ints[0].latitude, 0.0)
+        self.assertAlmostEqual(ints[0].longitude, 50.0)
+        self.assertEqual(ints[1], SurfacePoint(0.0, 230.0))
+        #path1 = (SurfacePoint(51.5,0.1), SurfacePoint(40.7, 74.0)) # London - New York
+        #path2 = (SurfacePoint(46.8, 71.2), SurfacePoint(44.6, 63.5)) # Quebec - Halifax
+        #self.assertEqual(self.ps.gcIntersections(path1, path2)[1], SurfacePoint(45.2, 65.4).vector())
+
+    def testPathsIntersectNoWrapNoSingularity(self):
+        path1 = (SurfacePoint(50, 20), SurfacePoint(-50, 80))
+        path2 = (SurfacePoint(-50, 20), SurfacePoint(50, 80))
+        self.assertTrue(self.ps.pathsIntersect(path1, path2))
+        # Positive case
+
+    def testPathsDontIntersectNoWrapNoSingularity(self):
+        path1 = (SurfacePoint(50, 20), SurfacePoint(-50, 80))
+        path2 = (SurfacePoint(-50, 20), SurfacePoint(50, 10))
+        self.assertFalse(self.ps.pathsIntersect(path1, path2))
+
+    def testPathsDontIntersectAntipodeOnOneArc(self):
+        path1 = (SurfacePoint(50, 20), SurfacePoint(-50, 80))
+        path2 = (SurfacePoint(-50, 20), SurfacePoint(-30, 30))
+        self.assertFalse(self.ps.pathsIntersect(path1, path2))
+        
+        
+    def testPathsIntersectOnMeridian(self):
+        path1 = (SurfacePoint(0,300), SurfacePoint(0,40))
+        path2 = (SurfacePoint(-40, 0), SurfacePoint(40, 0))
+        self.assertTrue(self.ps.pathsIntersect(path1, path2))
+        # Negative - antipodes on neither arc
+        # Negative - antipodes on one are
+        # Edge cases
 
     def testPointInRegion(self):
         pass
