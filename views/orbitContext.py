6e17be61f13c2b87a685e05ab4d9cebfedc84975
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-03-29 00:08:40 +0100

    Basic orbit drawing

--- views/orbitContext.py
+++ views/orbitContext.py
@@ -16,9 +16,24 @@ from pygame.locals import (
 
 LOADMENUVIEW = pygame.USEREVENT + 2
 
+class OrbitNode(pygame.sprite.Sprite):
+    def __init__(self, center=(0, 0)):
+        super(OrbitNode, self).__init__()
+        self.center = center
+        self.surf = pygame.surface.Surface((100, 100))
+        self.surf.set_colorkey((0, 0, 0))
+        pygame.draw.circle(self.surf, (255, 255, 100, 100), (10, 10), 10.0)
+
+        self.rect = self.surf.get_rect(center = self.center)
+
+
 class OrbitContext(GUIContext):
     def __init__(self, screen, model):
         super(OrbitContext, self).__init__(screen, model)
+        sun = OrbitNode((200, 200))
+
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(sun)
     
     def run(self):
         returnCode = 0
@@ -35,11 +50,12 @@ class OrbitContext(GUIContext):
                     returnCode = LOADMENUVIEW
                     break
 
-        self.screen.fill((250, 206, 100))
+        self.screen.fill((20, 20, 120))
 
         surf = pygame.surface.Surface((50, 50))
         pygame.draw.rect(self.screen, (10, 10, 10), pygame.Rect(0, 0, 50, 50))
-     
 
+        for entity in self.all_sprites:
+            self.screen.blit(entity.surf, entity.rect)
 
         return returnCode
