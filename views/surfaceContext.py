d73a75d51fa7d03c4b69e8a9a8ff1bc93c16f64d
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-04-13 00:06:42 +0100

    Very basic pixel drawing for surface

--- views/surfaceContext.py
+++ views/surfaceContext.py
@@ -1,6 +1,7 @@
 from views.guiContext import GUIContext
 
 import pygame
+import math
 
 from pygame.locals import (
     RLEACCEL,
@@ -16,9 +17,33 @@ from pygame.locals import (
 
 LOADSURFACEVIEW = pygame.USEREVENT + 3
 
+
+center = (500, 400)
+radius = 300
+
 class SurfaceContext(GUIContext):
     def __init__(self, screen, model, manager):
         super(SurfaceContext, self).__init__(screen, model, manager)
+        self.surf = pygame.Surface((1200, 800))
+        self.surf.fill((50, 50, 50))
+        pxArray = pygame.surfarray.pixels3d(self.surf)
+        shape = pxArray.shape
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                x = i - center[0]
+                y = j - center[1]
+                measure = (x*x + y*y)
+                r = math.sqrt(measure)
+                if r < 300:
+                    pxArray[i, j, 0] = 200
+                    pxArray[i, j, 1] = 100
+                    pxArray[i, j, 2] = 10
+                else:
+                    pxArray[i, j, 0] = 10
+                    pxArray[i, j, 1] = 10
+                    pxArray[i, j, 2] = 10
+
+        del pxArray
 
     def run(self):
         returnCode = 0
@@ -28,6 +53,9 @@ class SurfaceContext(GUIContext):
                 returnCode = QUIT
                 break
 
-        self.screen.fill((200, 135, 250))
+
+
+        self.screen.blit(self.surf, pygame.Rect(0, 0, 1200, 800))
+        
 
         return returnCode
