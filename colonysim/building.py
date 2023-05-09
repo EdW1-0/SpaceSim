62763740cd4d1986b1095fe87a503f040d286caa
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-05-10 00:50:50 +0100

    Implemented StorageBuilding

--- colonysim/building.py
+++ colonysim/building.py
@@ -34,4 +34,50 @@ class ProductionBuilding(Building):
     def __init__(self, *args, **kwargs):
         super(ProductionBuilding, self).__init__(*args, **kwargs)
 
+class StorageBuilding(Building):
+    def __init__(self, id, buildingClass):
+        super(StorageBuilding, self).__init__(id, buildingClass)
+        self.amount = 0
+        self.contents = next(iter(buildingClass.stores))
+        
+    def setContents(self, id):
+        self.buildingClass.stores[id]
+        if self.amount > 0:
+            raise ValueError
+        self.contents = id
+
+    def capacity(self):
+        return self.buildingClass.stores[self.contents]
+    
+    def add(self, amount):
+        if not (isinstance(amount, dict)):
+            raise TypeError
+        elif not (len(amount) == 1):
+            raise ValueError
+        
+        value = amount[self.contents]
+        if value <= (self.capacity() - self.amount):
+            self.amount += value
+            excess = 0
+        else:
+            excess = value - (self.capacity() - self.amount)
+            self.amount = self.capacity()
+
+        return excess      
+    
+    def remove(self, amount):
+        if not (isinstance(amount, dict)):
+            raise TypeError
+        elif not (len(amount) == 1):
+            raise ValueError
+        
+        value = amount[self.contents]
+        supply = min(value, self.amount)
+        self.amount -= supply
+
+        return supply
+
+        
+
+
 
--- tests/test_colony/test_building.py
+++ tests/test_colony/test_building.py
@@ -1,7 +1,7 @@
 import unittest
 
-from colonysim.building import Building, BuildingStatus, BuildingStatusError, ProductionBuilding
-from colonysim.buildingClass import BuildingClass, ProductionBuildingClass
+from colonysim.building import Building, BuildingStatus, BuildingStatusError, ProductionBuilding, StorageBuilding
+from colonysim.buildingClass import BuildingClass, ProductionBuildingClass, StorageBuildingClass
 
 class TestBuilding(unittest.TestCase):
     def setUp(self):
@@ -57,6 +57,70 @@ class TestProductionBuilding(unittest.TestCase):
     def testProductionBuildingConstructor(self):
         self.assertTrue(ProductionBuilding(3, self.pbc))
 
+class TestStorageBuilding(unittest.TestCase):
+    def setUp(self):
+        self.sbc = StorageBuildingClass("MOCK", "Mock", stores={"O2": 1000, "H2": 8000})
+
+    def testStorageBuilding(self):
+        self.assertTrue(StorageBuilding)
+
+    def testStorageBuildingConstructor(self):
+        self.assertTrue(StorageBuilding(7, self.sbc))
+        
+    def testStorageBuildingAttributes(self):
+        sb = StorageBuilding(5, self.sbc)
+        self.assertTrue(hasattr(sb, "amount"))
+        self.assertTrue(hasattr(sb, "contents"))
+
+    def testStorageBuildingSetContents(self):
+        sb = StorageBuilding(3, self.sbc)
+        
+        self.assertEqual(sb.contents, "O2")
+        sb.setContents("H2")
+        self.assertEqual(sb.contents, "H2")
+
+        with self.assertRaises(KeyError):
+            sb.setContents("CO2")
+
+        sb.amount = 5
+        with self.assertRaises(ValueError):
+            sb.setContents("H2")
+
+    def testStorageBuildingCapacity(self):
+        sb = StorageBuilding(4, self.sbc)
+        self.assertEqual(sb.capacity(), 1000)
+        sb.setContents("H2")
+        self.assertEqual(sb.capacity(), 8000)
+        
+    def testStorageBuildingAddMaterial(self):
+        sb = StorageBuilding(6, self.sbc)
+        sb.setContents("H2")
+        self.assertEqual(sb.add({"H2": 6000}), 0)
+        self.assertEqual(sb.amount, 6000)
+        self.assertEqual(sb.add({"H2": 3000}), 1000)
+        self.assertEqual(sb.amount, 8000)
+        with self.assertRaises(KeyError):
+            sb.add({"O2": 1000})
+        with self.assertRaises(TypeError):
+            sb.add(1000)
+        with self.assertRaises(ValueError):
+            sb.add({"H2": 100, "O2": 100})
+
+    def testStorageBuildingRemoveMaterial(self):
+        sb = StorageBuilding(8, self.sbc)
+        sb.setContents("O2")
+        sb.add({"O2": 1000})
+        self.assertEqual(sb.remove({"O2": 200}), 200)
+        self.assertEqual(sb.amount, 800)
+        self.assertEqual(sb.remove({"O2": 1400}), 800)
+        self.assertEqual(sb.amount, 0)
+
+        with self.assertRaises(KeyError):
+            sb.remove({"H2": 100})
+        with self.assertRaises(TypeError):
+            sb.remove(100)
+        with self.assertRaises(ValueError):
+            sb.remove({"H2": 10, "O2": 10})
 
 
 
