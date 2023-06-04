2be4e7af2c7591db66da9931fcaa6820d06a0454
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-06-04 22:52:36 +0100

    Implemented vehicle list for ColonyContext

--- views/colonyContext.py
+++ views/colonyContext.py
@@ -1,9 +1,18 @@
 from views.guiContext import GUIContext
+from views.sidePanels.sideStatusPanel import SideStatusPanel
+from views.timingView import TimingPanel
+
+from views.sidePanels.colonyStatusPanels import ColonyTabPanel, ColonyVehiclePanel
 
 from colonysim.colony import Colony
 
 import pygame
-from pygame_gui.elements import UILabel
+from pygame_gui.elements import UILabel, UIButton
+from pygame_gui  import (
+    UI_BUTTON_PRESSED,
+    UI_SELECTION_LIST_NEW_SELECTION,
+    UI_BUTTON_ON_HOVERED
+)
 
 from pygame.locals import (
     RLEACCEL,
@@ -17,6 +26,7 @@ from pygame.locals import (
     QUIT,
 )
 
+
 class ColonyContext(GUIContext):
     def __init__(self, screen, model, manager, colony):
         super().__init__(screen, model, manager)
@@ -25,6 +35,16 @@ class ColonyContext(GUIContext):
         self.colony_name_label = UILabel(pygame.Rect(400,200,100, 100), 
                                          text="Colony name placeholder", 
                                          manager=manager)
+        summary_rect = pygame.Rect(800, 300, 400, 600)
+        tab_rect = pygame.Rect(800, 200, 400, 100)
+        timing_rect = pygame.Rect(800, 0, 400, 200)
+        
+        self.tab_panel = ColonyTabPanel(tab_rect, manager=manager)
+        self.timing_panel = TimingPanel(timing_rect, manager = manager, timingMaster=model.timingMaster)
+
+        self.vehicle_panel = ColonyVehiclePanel(summary_rect, manager=manager, colony=colony)
+
+        self.active_panel = None
 
     def run(self):
         returnCode = 0
@@ -33,6 +53,21 @@ class ColonyContext(GUIContext):
             if event.type == QUIT:
                 returnCode = QUIT
                 break
+            if event.type == UI_BUTTON_PRESSED:
+                if self.timing_panel.handle_event(event):
+                    pass
+                elif self.tab_panel.handle_event(event):
+                    if self.tab_panel.upperEvent == 3:
+                        if self.active_panel:
+                            self.active_panel.hide()
+                        self.active_panel = self.vehicle_panel
+                        self.vehicle_panel.update()
+                        self.active_panel.show()
+                    pass
+                elif self.active_panel and self.active_panel.handle_event(event):
+                    pass
+
+            self.manager.process_events(event)
 
         self.colony_name_label.set_text(self.colony.name)
 
---
+++ views/sidePanels/colonyStatusPanels.py
@@ -0,0 +1,55 @@
+import pygame
+
+from views.sidePanels.sideStatusPanel import SideStatusPanel
+
+from pygame_gui.elements import UIButton, UIImage, UILabel, UITextBox, UISelectionList
+
+class ColonyTabPanel(SideStatusPanel):
+    def __init__(self, rect, manager=None):
+        super().__init__(rect, manager)
+        self.target_button = UIButton(pygame.Rect(0, 0, 100, 100), 
+                                          text="Construction",  
+                                          container = self.container, 
+                                          manager=manager)
+        
+        self.production_button = UIButton(pygame.Rect(100, 0, 100, 100), 
+                                          text="Production",  
+                                          container = self.container, 
+                                          manager=manager)
+        
+        self.vehicles_button = UIButton(pygame.Rect(200, 0, 100, 100), 
+                                          text="Vehicles",  
+                                          container = self.container, 
+                                          manager=manager)
+
+        self.ships_button = UIButton(pygame.Rect(300, 0, 100, 100), 
+                                          text="Ships",  
+                                          container = self.container, 
+                                          manager=manager)
+        
+    def handle_event(self, event):
+        self.upperEvent = 0
+        if super().handle_event(event):
+            return True
+        elif event.ui_element == self.vehicles_button:
+            self.upperEvent = 3
+            return True
+        else:
+            return False
+        
+class ColonyVehiclePanel(SideStatusPanel):
+    def __init__(self, rect, manager=None, colony=None):
+        super().__init__(rect, manager)
+        self.colony = colony
+
+        vehicles_text = "Vehicles"
+        self.vehicles_text = UILabel(pygame.Rect(0, 50, 400, 50), text=vehicles_text, manager=manager, container=self.container)
+
+        self.vehicle_list = UISelectionList(pygame.Rect(0, 100, 400, 500),
+                                            [("foo", "bar")],
+                                            manager=manager,
+                                            container=self.container)
+        
+    def update(self):
+        self.vehicle_list.set_item_list([vehicle.name for vehicle in self.colony.vehicles.values()])
+        self.vehicle_list.show()
