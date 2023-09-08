ca7422ed3ff941b598d20314a001439661250f45
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-09-09 00:48:15 +0100

    Added basic cargo logic to ship

--- colonysim/ship.py
+++ colonysim/ship.py
@@ -1,13 +1,39 @@
 class Ship:
-    def __init__(self, id, name, shipClass, locale = None, deltaV=0):
+    def __init__(self, id, name, shipClass, locale = None, deltaV=0, cargo = None):
         self.id = id
         self.name = name
         self.shipClass = shipClass
         self.locale = locale
         self.dv = deltaV
+        if cargo is None:
+            self.cargo = {}
+        else:
+            self.cargo = cargo
 
     def deltaV(self):
         return self.dv
     
     def burnDeltaV(self, deltaV):
         self.dv -= deltaV
+
+    def addCargo(self, cargo):
+        for commodity in cargo:
+            if commodity in self.cargo:
+                self.cargo[commodity] += cargo[commodity]
+            else:
+                self.cargo[commodity] = cargo[commodity]
+
+    def removeCargo(self, cargo):
+        retDict = {}
+        for commodity in cargo:
+            if commodity in self.cargo:
+                if cargo[commodity] <= self.cargo[commodity]:
+                    self.cargo[commodity] -= cargo[commodity]
+                    retDict[commodity] = cargo[commodity]
+                else:
+                    retDict[commodity] = self.cargo[commodity]
+                    del self.cargo[commodity]
+            else:
+                retDict[commodity] = 0
+
+        return retDict
--- tests/test_colony/test_ship.py
+++ tests/test_colony/test_ship.py
@@ -19,3 +19,25 @@ class TestShip(unittest.TestCase):
         self.assertTrue(hasattr(s, "name"))
         self.assertTrue(hasattr(s, "deltaV"))
         self.assertTrue(hasattr(s, "shipClass"))
+
+class TestShipCargo(unittest.TestCase):
+    def setUp(self):
+        self.sc = ShipClass("TEST", "Test", 100)
+        self.s = Ship(0, "Ship 1", self.sc)
+
+    def testAddCargo(self):
+        self.s.addCargo({"H2O": 500, "BAUXITE": 100})
+        self.assertEqual(self.s.cargo["BAUXITE"], 100)
+        self.assertEqual(self.s.cargo["H2O"], 500)
+        self.s.addCargo({"H2O": 10000})
+        self.assertEqual(self.s.cargo["H2O"], 10500)
+
+    def testRemoveCargo(self):
+        self.s.addCargo({"NH3": 5000, "CH4": 2000})
+        retDict = self.s.removeCargo({"NH3":500, "CH4": 3000, "H2": 200})
+        self.assertEqual(retDict["NH3"], 500)
+        self.assertEqual(retDict["CH4"], 2000)
+        self.assertEqual(retDict["H2"], 0)
+        with self.assertRaises(KeyError):
+            self.s.cargo["CH4"]
+        self.assertEqual(self.s.cargo["NH3"], 4500)
