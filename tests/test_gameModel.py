eac5e35ed37f55a913aad87e44022b9028e54dfb
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-11-30 23:09:48 +0000

    Implemented unit test on save/load

--- tests/test_gameModel.py
+++ tests/test_gameModel.py
@@ -2,6 +2,9 @@ from gameModel import GameModel
 
 import unittest
 
+import os
+import tempfile
+
 
 class TestGameModelModule(unittest.TestCase):
     def testGameModel(self):
@@ -29,7 +32,7 @@ class TestGameModelClass(unittest.TestCase):
         self.assertFalse(self.gm.get_init())
 
 
-class TestGameModelLoading(unittest.TestCase):
+class TestGameModelNewGame(unittest.TestCase):
     def setUp(self):
         self.gm = GameModel()
 
@@ -40,3 +43,34 @@ class TestGameModelLoading(unittest.TestCase):
     def testGameModelFileNotFound(self):
         with self.assertRaises(FileNotFoundError):
             self.gm.load("not_a_json_path")
+
+class TestGameModelLoadSave(unittest.TestCase):
+    def setUp(self):
+        self.gm = GameModel()
+        self.file = tempfile.NamedTemporaryFile()
+
+
+    def testGameModelFileSaveLoad(self):
+        self.assertTrue(os.path.exists(self.file.name))
+        self.assertEqual(os.path.getsize(self.file.name), 0)
+        self.gm.load()
+        for i in range(17):
+            self.gm.timingMaster.step()
+            self.gm.tick()
+
+        self.assertEqual(self.gm.timingMaster.timestamp, 17)
+        
+        self.gm.saveModel(self.file)
+
+        self.assertTrue(os.path.exists(self.file.name))
+        self.assertGreater(os.path.getsize(self.file.name), 0)
+        self.file.seek(0)
+
+        newModel = GameModel()
+        self.assertFalse(newModel.init)
+        newModel.loadModel(self.file)
+        self.assertTrue(newModel.init)
+        self.assertEqual(newModel.timingMaster.timestamp, 17)
+
+    def tearDown(self):
+        self.file.close()
