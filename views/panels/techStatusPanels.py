208a8f3237041f1cd14add26bc1b070c4d78195d
EdW1-0 <edmundwarrick@hotmail.co.uk>
2023-12-10 23:21:04 +0000

    Added ancestor techs to tech panel

--- views/panels/techStatusPanels.py
+++ views/panels/techStatusPanels.py
@@ -28,6 +28,8 @@ class TechStatusPanel(SideStatusPanel):
                                   container=self.container)
         self.tech: TechNode = None
 
+        self.model = model
+
         self.cost_label = UILabel(pygame.Rect(100, 50, 200, 50), 
                                   "<Tech cost placeholder>", 
                                   manager=manager, 
@@ -70,5 +72,7 @@ class TechStatusPanel(SideStatusPanel):
             self.name_label.set_text(self.tech.name)
             self.cost_label.set_text(str(self.tech.cost))
             self.description_text.set_text(str(self.tech.description))
+
+            self.requires_list.set_item_list([self.model.techTree.nodeById(a).name for a in self.tech.ancestors])
             
 
