28cc4cc71c6b50695391dbce02608e9a5a0622cc
EdW1-0 <edmundwarrick@hotmail.co.uk>
2024-01-20 00:46:56 +0000

    Further fileLoad testing to handle alt classes

--- tests/test_util/test_fileLoad.py
+++ tests/test_util/test_fileLoad.py
@@ -2,18 +2,36 @@ import unittest
 
 from utility import loadEntityFile
 
-class BasicMock:
-    def __init__(self, id: str, data: int):
-        self.id = id
-        self.data = data
 
 class TestLoadEntityFile(unittest.TestCase):
     def testLoadEntityFile(self):
         self.assertTrue(loadEntityFile)
 
     def testLoadEntityFileBasic(self):
+        class BasicMock:
+            def __init__(self, id: str, data: int):
+                self.id = id
+                self.data = data
+
         basic = loadEntityFile(path="test_json/test_fileLoad", id="Basic", EntityClass=BasicMock)
         self.assertTrue(basic)
         self.assertEqual(len(basic), 3)
         self.assertEqual(basic["A"].id, "A")
         self.assertEqual(basic["A"].data, 1)
+
+    def testLoadEntityFileAltClasS(self):
+        class NormMock:
+            def __init__(self, id: str, data: int):
+                self.id = id
+                self.data = data
+
+        class AltMock(NormMock):
+            def __init__(self, *args, altie, **kwargs):
+                super(AltMock, self).__init__(*args, **kwargs)
+                self.altie = altie
+
+        alt = loadEntityFile(path="test_json/test_fileLoad", id="AltClass", EntityClass=NormMock, altClasses={"altie": AltMock})
+        self.assertTrue(alt)
+        self.assertTrue(isinstance(alt["NORMAL"], NormMock))
+        self.assertTrue(isinstance(alt["ALT"], AltMock))
+        self.assertEqual(alt["ALT"].altie["Q"], 7)
