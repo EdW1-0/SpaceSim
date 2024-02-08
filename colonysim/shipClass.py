c5de99dea6ef26bd8cfadaaad7693d1c9a07992f
EdW1-0 <edmundwarrick@hotmail.co.uk>
2024-02-08 22:18:35 +0000

    Add constructionCost and constructionTime to ShipClass

--- colonysim/shipClass.py
+++ colonysim/shipClass.py
@@ -1,5 +1,16 @@
 class ShipClass:
-    def __init__(self, id, name, maxDeltaV=0):
+    def __init__(self, id, name, maxDeltaV=0, constructionTime = 1, constructionCost = {}):
         self.id = id
         self.name = name
         self.maxDeltaV = maxDeltaV
+
+        self.baseConstructionCost = constructionCost
+        self.baseConstructionTime = constructionTime
+
+    def constructionCost(self):
+        return self.baseConstructionCost
+    
+    def constructionTime(self):
+        return self.baseConstructionTime
+
+
--- tests/test_colony/test_shipClass.py
+++ tests/test_colony/test_shipClass.py
@@ -10,9 +10,22 @@ class TestShipClass(unittest.TestCase):
     def testShipClassConstructor(self):
         self.assertTrue(ShipClass("LIFTER", "Lifter"))
         self.assertTrue(ShipClass("LIFTER", "Lifter", maxDeltaV=1000))
+        self.assertTrue(ShipClass("LIFTER", "Lifter", maxDeltaV=1000, constructionTime=100, constructionCost={"AL": 100}))
+
 
     def testShipClassAttributes(self):
         sc = ShipClass("LIFTER", "Lifter")
         self.assertTrue(hasattr(sc, "name"))
         self.assertTrue(hasattr(sc, "id"))
         self.assertTrue(hasattr(sc, "maxDeltaV"))
+        self.assertTrue(hasattr(sc, "baseConstructionCost"))
+        self.assertTrue(hasattr(sc, "baseConstructionTime"))
+
+class TestShipClassParameters(unittest.TestCase):
+    def setUp(self):
+        self.sc = ShipClass("LIFTER", "Lifter", maxDeltaV=1000, constructionCost={"AL": 10}, constructionTime=100)
+
+    def testShipClassConstructionParams(self):
+        self.assertEqual(self.sc.constructionTime(), 100)
+        self.assertEqual(self.sc.constructionCost(), {"AL": 10})
+        
