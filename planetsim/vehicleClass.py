6cd5521ac2f4e6793b7a63e5078d715189cd093b
EdW1-0 <edmundwarrick@hotmail.co.uk>
2024-02-24 23:28:30 +0000

    Extended VehicleClass to add construction params

--- planetsim/vehicleClass.py
+++ planetsim/vehicleClass.py
@@ -1,7 +1,16 @@
 class VehicleClass:
-    def __init__(self, id, name, maxFuel=0, maxV=0, fuelPerM=0):
+    def __init__(self, id, name, maxFuel=0, maxV=0, fuelPerM=0, constructionTime=1, constructionCost = {}):
         self.id = id
         self.name = name
         self.maxFuel = maxFuel
         self.maxV = maxV
         self.fuelPerM = fuelPerM
+
+        self.baseConstructionCost = constructionCost
+        self.baseConstructionTime = constructionTime
+
+    def constructionCost(self):
+        return self.baseConstructionCost
+    
+    def constructionTime(self):
+        return self.baseConstructionTime
--- tests/test_planet/test_vehicleClass.py
+++ tests/test_planet/test_vehicleClass.py
@@ -12,6 +12,7 @@ class TestVehicleClass(unittest.TestCase):
         self.assertTrue(VehicleClass("ROVER", "Rover", maxFuel=1000))
         self.assertTrue(VehicleClass("ROVER", "Rover", maxFuel=1000, maxV=2.0))
         self.assertTrue(VehicleClass("ROVER", "Rover", maxFuel=1000, fuelPerM=5.0))
+        self.assertTrue(VehicleClass("ROVER", "Rover", maxFuel=1000, maxV=2.0, fuelPerM=5.0, constructionTime=100, constructionCost={"AL": 100}))
 
     def testVehicleClassAttributes(self):
         vc = VehicleClass("ROVER", "Rover")
@@ -20,3 +21,16 @@ class TestVehicleClass(unittest.TestCase):
         self.assertTrue(hasattr(vc, "maxV"))
         self.assertTrue(hasattr(vc, "maxFuel"))
         self.assertTrue(hasattr(vc, "fuelPerM"))
+        self.assertTrue(hasattr(vc, "baseConstructionCost"))
+        self.assertTrue(hasattr(vc, "baseConstructionTime"))
+
+class TestVehicleClassParameters(unittest.TestCase):
+    def setUp(self):
+        self.vc = VehicleClass("ROVER", "Rover", maxFuel=1000, maxV=2.0, fuelPerM=5.0, constructionCost={"AL": 10}, constructionTime=100)
+
+    def testVehicleClassConstructionParams(self):
+        self.assertEqual(self.vc.maxV, 2.0)
+        self.assertEqual(self.vc.maxFuel, 1000)
+        self.assertEqual(self.vc.fuelPerM, 5.0)
+        self.assertEqual(self.vc.constructionTime(), 100)
+        self.assertEqual(self.vc.constructionCost(), {"AL": 10})
