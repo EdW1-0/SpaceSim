c704ea41d06473ad1517a8b38724ca0b41477443
EdW1-0 <edmundwarrick@hotmail.co.uk>
2024-03-01 01:24:39 +0000

    People file loading

--- peoplesim/peopleSim.py
+++ peoplesim/peopleSim.py
@@ -1,2 +1,6 @@
+from utility import loadEntityFile
+from peoplesim.person import Person
+
 class PeopleSim:
-    pass
+    def __init__(self, jsonPath: str = "json/people"):
+        self._people = loadEntityFile(jsonPath, "People", Person)
--- tests/test_person/test_peopleSim.py
+++ tests/test_person/test_peopleSim.py
@@ -6,3 +6,9 @@ class TestPeopleSim(unittest.TestCase):
     def testPeopleSim(self):
         self.assertTrue(PeopleSim)
         self.assertTrue(PeopleSim())
+
+    def testPeopleSimConstructor(self):
+        self.assertTrue(PeopleSim(jsonPath="test_json/test_people"))
+
+    def testPeopleSimAttributes(self):
+        self.assertTrue(hasattr(PeopleSim(), "_people"))
